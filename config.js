47420b6fe462f35d2a28804f07ef6e76c1c4c612
Szunder123 <szymonwie@interia.pl>
2020-10-18 19:00:39 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "NzMwMzYwMTczOTg1NzkyMDUy.XwWW6g.V-iIDJECye0FqbPBB1rtwc4UMDo",
+    prefix: "$",
+    owner: "622691164843409418",
+    corona_api: "https://corona.lmao.ninja/v2"
+    }
